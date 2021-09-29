a77ee14baaad92addc3d053608d2d28f4ed2983c
Pablo Santos <pablojs2008@hotmail.com>
2021-09-29 12:13:22 -0300

    Create home page

--- app/src/main/java/com/example/superquiz/activitys/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/superquiz/activitys/QuizQuestionActivity.kt
@@ -1,21 +1,12 @@
 package com.example.superquiz.activitys
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
-import android.widget.Button
-import android.widget.Toast
 import com.example.superquiz.R
 
-class MainActivity : AppCompatActivity() {
+class QuizQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_quiz_question)
     }
-
-
-
 }
